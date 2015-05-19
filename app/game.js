943bf657552a83d98a526d3ad12254038a2a5233
Antonio Monge <amonge@nearsoft.com>
2015-05-18 23:11:20 -0700

    Added splash screen.

--- app/game.js
+++ app/game.js
@@ -4,4 +4,5 @@ var game = new Phaser.Game(350, 650, Phaser.AUTO, 'game');
 
 game.state.add('play', play);
 game.state.add('gameOver', gameOver);
-game.state.start('play');
+game.state.add('bootGame', bootGame);
+game.state.start('bootGame');
